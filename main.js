be237a1c45ae9c353f143b2b03dbca6be3334aae
jiamax <zz12345zxc@gmail.com>
2022-04-24 17:02:05 +0800

    final http setting

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const port = 3000;
+var fs = require('fs');
+var http = require('http');
+var httpServer = http.createServer(app);
+// For http
+// httpServer.listen(3000,'0.0.0.0');
+
 
 // 讓html可以使用js和css
 app.use(express.static(__dirname));
 
 app.get('/',(req, res) => {
+    res.header('Content-type', 'text/html');
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
